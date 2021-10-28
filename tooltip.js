392c93a658778453a9bf293179a9a25fe620f627
gahgsp <kelvinbianchi@gmail.com>
2021-10-27 21:03:09 -0300

    Added the first event listeners inside the Web Component.

--- tooltip.js
+++ tooltip.js
@@ -1,7 +1,32 @@
 class Tooltip extends HTMLElement {
+  /**
+   * Constructor is used only for basic initializations.
+   */
   constructor() {
     super();
-    console.log('The Tooltip Web Component is working!');
+    this._container;
+  }
+
+  /**
+   * This lifecycle hook runs once the element is attached to the DOM.
+   * The correct place for DOM initializations.
+   */
+  connectedCallback() {
+    const icon = document.createElement('span');
+    icon.textContent = ' (?)';
+    icon.addEventListener('mouseenter', this._showTooltip.bind(this));
+    icon.addEventListener('mouseleave', this._hideTooltip.bind(this));
+    this.appendChild(icon);
+  }
+
+  _showTooltip() {
+    this._container = document.createElement('div');
+    this._container.textContent = 'This is the Tooltip Web Component text!';
+    this.appendChild(this._container);
+  }
+
+  _hideTooltip() {
+    this.removeChild(this._container);
   }
 }
 
